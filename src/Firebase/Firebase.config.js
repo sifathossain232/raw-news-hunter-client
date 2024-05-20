de981a606b36bee160d8013a7da61ebdb741bba1
SIFAT HOSSAIN <143956074+sifathossain232@users.noreply.github.com>
2024-05-20 11:14:17 +0600

    firebase api key update

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -1,3 +1,20 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   VITE_APIKEY: import.meta.env.VITE_APIKEY,
+//   VITE_AUTHDOMAIN: import.meta.env.VITE_APIKEY,
+//   VITE_PROJECTID: import.meta.env.VITE_APIKEY,
+//   VITE_STORAGEBUCKET: import.meta.env.VITE_APIKEY,
+//   VITE_MESSAGINGSENDERID: import.meta.env.VITE_APIKEY,
+//   VITE_APPID: import.meta.env.VITE_APIKEY
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -5,12 +22,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_APIKEY,
-  projectId: import.meta.env.VITE_APIKEY,
-  storageBucket: import.meta.env.VITE_APIKEY,
-  messagingSenderId: import.meta.env.VITE_APIKEY,
-  appId: import.meta.env.VITE_APIKEY
+  apiKey: "AIzaSyAdsfLZnbn2AM08rngFSVjUTIZpJmMLS4c",
+  authDomain: "raw-news-hunter.firebaseapp.com",
+  projectId: "raw-news-hunter",
+  storageBucket: "raw-news-hunter.appspot.com",
+  messagingSenderId: "238215341096",
+  appId: "1:238215341096:web:de9412573303765199c11d"
 };
 
 // Initialize Firebase
